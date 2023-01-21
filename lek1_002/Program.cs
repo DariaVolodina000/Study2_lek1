657aebaf53fe8be616e9a980ca57d4150df74b0a
Daria <dashavolodina@gmail.com>
2023-01-21 17:45:16 +0800

    Рабочая версия Дни недели

---
+++ lek1_002/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите номер дня");
+string[] week = new string [] {"Понедельник", "Вторник" , "Среда", "Четверг", "Это ПЯТНИЦА!", "Суббота", "Воскресенье"};
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(week [num-1]);
+
+
+
+
